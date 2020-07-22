666b9b0fa7f9873526727d8af7beb55626897ef7
Joe Dai <dm.joedai@gmail.com>
2020-07-21 23:30:22 -0700

    Initial version - base features complete

---
+++ settings.example.h
@@ -0,0 +1,10 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+const char* SSL_FINGERPRINT = "";  // Torn SSL cert's SHA1 print
+const char* API_KEY = "";          // Torn API key
+
+const char* WIFI_SSID = "";        // WiFi SSID
+const char* WIFI_PASSWORD = "";    // WiFi password
+
+#endif
