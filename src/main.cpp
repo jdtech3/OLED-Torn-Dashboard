a24d71c162bc1f895b967041c62a6a935bcfe39f
Joe Dai <dm.joedai@gmail.com>
2020-07-22 20:07:50 -0700

    Add J logo on boot, centered destination text, travelling animation (w/ arrow) and various refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include <Arduino.h>
 
 #include <Wire.h>
@@ -12,9 +14,22 @@
 ADC_MODE(ADC_VCC);      // for boot-up Vcc display
 Adafruit_SSD1306 display(128, 64, &Wire, -1);  // height, width, lib, reset pin
 
-uint64_t timeNow = 0;   // for (more) accurate loop delay
-uint16_t timer;         // travel timer
-String state;           // player state (i.e. "Okay")
+uint64_t timeNow = 0;       // for (more) accurate loop delay
+uint8_t arrowAnimPos = 0;   // offset position of arrow animation
+uint16_t timer;             // travel timer
+String destination;         // travel destination
+String state;               // player state (i.e. "Okay")
+
+const unsigned char jLogoBitmap [] PROGMEM = {
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x00,
+	0x00, 0x00, 0xc0, 0x00, 0x00, 0x00, 0xe0, 0x00, 0x00, 0x00, 0xf0, 0x00, 0x00, 0x00, 0xf0, 0x00,
+	0x00, 0x40, 0xf0, 0x00, 0x00, 0xc0, 0xf2, 0x00, 0x01, 0xc0, 0xf3, 0x00, 0x03, 0xc0, 0xf3, 0x80,
+	0x07, 0xc0, 0xf3, 0xc0, 0x07, 0xc0, 0xf3, 0xc0, 0x07, 0x80, 0xf3, 0xc0, 0x07, 0x00, 0xf1, 0xc0,
+	0x07, 0x80, 0xf3, 0xc0, 0x07, 0xc0, 0xf3, 0xc0, 0x07, 0xc0, 0xf3, 0xc0, 0x03, 0xd0, 0xf3, 0xc0,
+	0x01, 0xd8, 0xf3, 0x80, 0x00, 0xdc, 0xf3, 0x00, 0x00, 0x5e, 0xf2, 0x00, 0x00, 0x1f, 0xf0, 0x00,
+	0x00, 0x1f, 0xf0, 0x00, 0x00, 0x0f, 0xf0, 0x00, 0x00, 0x07, 0xf0, 0x00, 0x00, 0x03, 0xf0, 0x00,
+	0x00, 0x01, 0xe0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
 
 // Small info data type
 struct TravelInfo {
@@ -28,6 +43,8 @@ struct TravelInfo getTravelInfo();
 
 
 void setup() {
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
     pinMode(0, INPUT_PULLUP);   // use FLASH button
 
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);     // I2C address
@@ -36,25 +53,30 @@ void setup() {
 
     display.setTextSize(1);               // set initial display settings
     display.setTextColor(WHITE, BLACK);
+    display.setCursor(0, 0);
+
+    char vccBuf[24];
+    sprintf(vccBuf, "Vcc: %.2fV", ESP.getVcc() / 1000.0);
+    display.println(vccBuf);
 
     // Connect to WiFi (continously displaying Vcc and connection status)
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
-        display.setCursor(0, 0);
-        display.print(ESP.getVcc() / 1000.0); display.println("V");
-
         display.setCursor(0, 48);
-        display.println(String("Connecting to: ") + WIFI_SSID);
+        display.println(String(F("Connecting to: ")) + WIFI_SSID);
         display.display();
 
         delay(500);
     }
 
-    // Display IP
+    // Display Logo, IP and Vcc
     display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println(vccBuf);
     display.setCursor(0, 48);
-    display.println("Connected.");
+    display.println(F("Connected."));
     display.println(String("IP: ") + WiFi.localIP().toString());
+    display.drawBitmap(48, 16, jLogoBitmap, 32, 32, WHITE);  // use own splash screen
     display.display();
 
     delay(1000);
@@ -63,13 +85,22 @@ void setup() {
     struct TravelInfo tInfo = getTravelInfo();
     timer = tInfo.timeLeft;
     state = tInfo.state;
+    destination = tInfo.destination;
 
     display.clearDisplay();
     display.setCursor(0, 0);
 
     // Travelling...
     if (timer > 0) {
-        display.println(String("> ") + tInfo.destination);
+        char buf[24];
+        sprintf(buf, "> %s <", destination.c_str());
+
+        // Center "> destination <" within screen (64px max width)
+        int16_t _s; uint16_t _u;
+        uint16_t textWidth; display.getTextBounds(buf, 0, 0, &_s, &_s, &textWidth, &_u);
+        display.setCursor((128 - textWidth) / 2, 0);
+
+        display.println(buf);
     }
     // Arrived at destination!
     else if (tInfo.destination != "Torn") {
@@ -77,8 +108,8 @@ void setup() {
     }
     // In Torn, not travelling
     else {
-        display.println("At: Torn City");
-        display.println("No travel detected :(");
+        display.println(F("At: Torn City"));
+        display.println(F("No travel detected :("));
     }
 
     display.display();  // display all of above
@@ -86,12 +117,6 @@ void setup() {
 
 void loop() {
     timeNow = millis();   // for timing
-
-    // Sleep indefinitely (until RESET pressed) if FLASH pressed
-    if (digitalRead(0) == LOW) {
-        display.clearDisplay();
-        ESP.deepSleep(0);
-    }
 
     // Set display settings for big text
     display.setTextSize(2);
@@ -116,17 +141,35 @@ void loop() {
     }
     // Not travelling... display status instead
     else {
-        display.setCursor(0, 20);   // move cursor slightly higher for extra line
+        display.setCursor(0, 24);
 
         display.setTextSize(1);
-        display.println("Status:");
+        display.println(F("Status:"));
         display.setTextSize(2);
         display.println(state);
     }
 
     display.display();  // update display
 
-    while (millis() < timeNow + 1000) {}  // loop checks for whether 1 sec has passed
+    // More complicated but more accurate (?) version of delay()
+    while (millis() < timeNow + 1000) {
+
+        // Play arrow animation if travelling
+        if (timer > 0) {
+            if (millis() % 500 == 0) {
+                char arrow;
+                destination == F("Torn") ? arrow = '<' : arrow = '>';   // make arrow point left when returning, right when going
+
+                display.setTextSize(1);
+                display.setCursor(51, 56); display.print(F("     "));   // clear existing
+                display.setCursor(51 + arrowAnimPos, 56);   // figure out where to put arrow
+                display.print(arrow);
+                display.display();
+
+                arrowAnimPos == 20 ? arrowAnimPos = 0 : arrowAnimPos += 5;  // check number = (# of arrows - 1) * 5
+            }
+        }
+    }
 }
 
 struct TravelInfo getTravelInfo() {
@@ -158,7 +201,7 @@ struct TravelInfo getTravelInfo() {
         TravelInfo ret = {
             doc["status"]["state"].as<String>(),
             doc["travel"]["destination"].as<String>(),
-            doc["travel"]["time_left"].as<int>()
+            doc["travel"]["time_left"].as<uint16_t>()
         };
         return ret;
     }
